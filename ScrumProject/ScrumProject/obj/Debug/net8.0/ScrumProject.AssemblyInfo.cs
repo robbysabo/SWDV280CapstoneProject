77d8f8042cd2c1f633ff4374dd155bca5ced472b
brandon-deaton <dream.cloud936@gmail.com>
2024-03-25 15:06:05 -0600

    y

--- ScrumProject/ScrumProject/obj/Debug/net8.0/ScrumProject.AssemblyInfo.cs
+++ ScrumProject/ScrumProject/obj/Debug/net8.0/ScrumProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScrumProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47263b9537c6a89e5e1e05bf15fb19869c244490")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b12de81503fc9b61d43b84c685ea23c6d94c95c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScrumProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScrumProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
