05808a47fdb20b5e7489c6ce6207a865eb0adac5
mtarpley@cwiinst.com <mtarpley@cwiinst.com>
2024-03-25 13:08:51 -0600

    test

--- ScrumProject/ScrumProject/obj/Debug/net8.0/ScrumProject.AssemblyInfo.cs
+++ ScrumProject/ScrumProject/obj/Debug/net8.0/ScrumProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScrumProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a886ec62c6680d759cb8b2aa354389465492d92")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad135482a20fd5621fdf487f3c07a99e2df7ce68")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScrumProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScrumProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
