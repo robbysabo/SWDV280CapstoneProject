3f48a83adf8a540f309bcdc9178af56a3e61ba80
Eduardo Saucedo <117676083+ESaucedo3@users.noreply.github.com>
2024-03-29 09:57:58 -0600

    Minor changes to contact page

--- ScrumProject/ScrumProject/Controllers/ContactController.cs
+++ ScrumProject/ScrumProject/Controllers/ContactController.cs
@@ -1,9 +1,18 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace ScrumProject.Controllers;
+namespace ScrumProject.Controllers {
+    public class ContactController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
 
-public class ContactController : Controller {
-    public IActionResult Index() {
-        return View();
+        [HttpPost]
+        public IActionResult Send() {
+            return RedirectToAction("Confirmation");
+        }
+
+        public IActionResult Confirmation() {
+            return View();
+        }
     }
 }
