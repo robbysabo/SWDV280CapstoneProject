e384f3a85f731962045e88502c86a1f01c6b0530
brandon-deaton <dream.cloud936@gmail.com>
2024-03-29 22:05:11 -0600

    added UI features and company logos

--- ScrumProject/ScrumProject/obj/Debug/net8.0/ScrumProject.AssemblyInfo.cs
+++ ScrumProject/ScrumProject/obj/Debug/net8.0/ScrumProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScrumProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77d8f8042cd2c1f633ff4374dd155bca5ced472b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73bbdbf60aaf27ca2026b48635e3654bcb6d39c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScrumProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScrumProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
