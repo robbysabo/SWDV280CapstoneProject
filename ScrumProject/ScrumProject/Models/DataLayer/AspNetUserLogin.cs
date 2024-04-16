fe336534acbcd96f3437190e2e6d2a933c4525c0
Michael <=>
2024-04-15 18:21:14 -0600

    Added AboutPg. Updated the database to replace 'Images' table with 'WorkerImages' because it makes more sense.

---
+++ ScrumProject/ScrumProject/Models/DataLayer/AspNetUserLogin.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace ScrumProject.Models.DataLayer;
+
+[PrimaryKey("LoginProvider", "ProviderKey")]
+[Index("UserId", Name = "IX_AspNetUserLogins_UserId")]
+public partial class AspNetUserLogin
+{
+    [Key]
+    public string LoginProvider { get; set; } = null!;
+
+    [Key]
+    public string ProviderKey { get; set; } = null!;
+
+    public string? ProviderDisplayName { get; set; }
+
+    public string UserId { get; set; } = null!;
+
+    [ForeignKey("UserId")]
+    [InverseProperty("AspNetUserLogins")]
+    public virtual AspNetUser User { get; set; } = null!;
+}
---
+++ ScrumProject/ScrumProject/Models/DataLayer/WorkerImage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace ScrumProject.Models.DataLayer;
+
+public partial class WorkerImage
+{
+    [Key]
+    [Column("ImageID")]
+    public int ImageId { get; set; }
+
+    [Unicode(false)]
+    public string ImageSrc { get; set; } = null!;
+
+    [Unicode(false)]
+    public string ImageAlt { get; set; } = null!;
+
+    [Unicode(false)]
+    public string? Description { get; set; }
+}
