991f88e6e0dab498ce75c881ec58ea81a56a382d
brycefish <bryce.k.fish@gmail.com>
2024-04-21 21:55:27 -0600

    added placeholder view/controller. Need to add to admin area

---
+++ ScrumProject/ScrumProject/Controllers/ViewApptController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ScrumProject.Models.DataAccess;
+using ScrumProject.Models.DataLayer;
+
+namespace ScrumProject.Controllers
+{
+    public class ViewApptController : Controller
+    {
+        private Repository<Appointment> appt { get; set; }
+
+        public ViewApptController(ScrumProjectContext ctx)
+        {
+            appt = new Repository<Appointment>(ctx);
+        }
+
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var upcomingAppointments = appt.List(new QueryOptions<Appointment> { OrderBy = a => a.RequestDate }).ToList();
+
+            return View(upcomingAppointments);
+
+        }
+    }
+}
