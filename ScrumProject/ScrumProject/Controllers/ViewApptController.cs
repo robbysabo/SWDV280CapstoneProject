d9933364c54d8faaff589ae32fd53b133d0d266f
brycefish <bryce.k.fish@gmail.com>
2024-04-21 22:39:22 -0600

    added VM

--- ScrumProject/ScrumProject/Controllers/ViewApptController.cs
+++ ScrumProject/ScrumProject/Controllers/ViewApptController.cs
@@ -7,20 +7,45 @@ namespace ScrumProject.Controllers
     public class ViewApptController : Controller
     {
         private Repository<Appointment> appt { get; set; }
+        //private Repository<AppointmentType> apptType { get; set; }
 
         public ViewApptController(ScrumProjectContext ctx)
         {
             appt = new Repository<Appointment>(ctx);
+            //apptType = new Repository<AppointmentType>(ctx);
         }
 
 
-        [HttpGet]
         public IActionResult Index()
         {
             var upcomingAppointments = appt.List(new QueryOptions<Appointment> { OrderBy = a => a.RequestDate }).ToList();
 
             return View(upcomingAppointments);
-
         }
+
+
+
+        //public IActionResult Index()
+        //{
+        //    var today = DateTime.Today.AddDays(-4);
+
+
+        //    //var upcomingAppointments = appt.List(new QueryOptions<Appointment> { OrderBy = a => a.RequestDate }).ToList();
+
+        //    ViewApptViewModel model = new ViewApptViewModel();
+        //    model.AppointmentTypes = apptType.List(new QueryOptions<AppointmentType> { OrderBy = a => a.AppointmentTypeId }).ToList();
+
+        //    //use types to create select list items
+        //    foreach (var type in model.AppointmentTypes)
+        //    {
+        //        model.TypeSelectList.Add(new SelectListItem { Text = type.Description, Value = type.AppointmentTypeId.ToString() });
+        //    }
+
+        //    // Set the SelectedAppointmentType property
+        //    model.SelectedAppointmentType = model.AppointmentTypes.FirstOrDefault();
+
+        //    return View(model);
+
+        //}
     }
 }
---
+++ ScrumProject/ScrumProject/ViewModels/ViewApptVieModel.cs
@@ -0,0 +1,23 @@
+﻿using ScrumProject.Models;
+using ScrumProject.Models.DataLayer;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
+
+namespace ScrumProject.ViewModels
+{
+    public class ViewApptViewModel
+    {
+
+        //public ViewApptViewModel()
+        //{
+        //    Appointment = new Appointment { AppointmentType = SelectedAppointmentType };
+        //}
+        //public Appointment Appointment { get; set; }
+        //public AppointmentType? SelectedAppointmentType { get; set; }
+        //public List<AppointmentType> AppointmentTypes { get; set; } = new List<AppointmentType>();
+        //public List<SelectListItem> TypeSelectList { get; set; } = new List<SelectListItem>();
+
+        //public IEnumerable<Appointment> User { get; set; }
+    }
+
+}
