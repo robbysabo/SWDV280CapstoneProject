3e0a78431d79ce367c8ca6e380f051069f9c23d4
mtarpley@cwiinst.com <mtarpley@cwiinst.com>
2024-04-24 14:54:28 -0600

    added ability to remove appointments

--- ScrumProject/ScrumProject/Areas/Admin/Controllers/ViewApptController.cs
+++ ScrumProject/ScrumProject/Areas/Admin/Controllers/ViewApptController.cs
@@ -33,5 +33,21 @@ namespace ScrumProject.Areas.Admin.Controllers
             return View(upcomingAppointments);
         }
 
+        //MWT: added remove appt action
+        public RedirectToActionResult RemoveAppt(int id)
+        {
+            //get appt set appt status to complete
+            Appointment selectedAppt = appt.Get(id);
+            selectedAppt.ApptStat = "C";
+
+            appt.Save();
+
+            //set tempdata for alert
+            TempData["Alert"] = $"Appointment for {selectedAppt.ContactFname}, {selectedAppt.ContactLname} on {selectedAppt.RequestDate.ToString("d")}" +
+                $" has been marked as complete.";
+
+            return RedirectToAction("Index");
+        }
+
     }
 }
